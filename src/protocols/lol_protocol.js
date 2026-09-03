01499269946426a370a7bd4cd945d30d9e090094
Neito112 <orenji11299@gmail.com>
2026-09-04 06:20:13 +0700

    feat: add League of Legends Tactical Protocol

--- src/protocols/lol_protocol.js
+++ src/protocols/lol_protocol.js
@@ -1,7 +1,7 @@
 ﻿const https = require('https');
 const http = require('http');
 
-const GEMINI_KEY = "AIzaSyCBtopxSMXhJYAoI0D_ytzQCut_LB67VXc";
+const GEMINI_KEY = "process.env.GEMINI_API_KEY || "YOUR_GEMINI_API_KEY"";
 
 const LOL_SYSTEM_PROMPT = `
 Bạn là CỐ VẤN CHIẾN THUẬT & HỆ THỐNG PHÂN TÍCH LIÊN MINH HUYỀN THOẠI (League of Legends Master Protocol - LOL).
